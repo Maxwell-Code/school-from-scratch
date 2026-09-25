d6e5efa4a5257ca734c7e090601bbac119a90440
Univyrsal <126922873+univyrsal@users.noreply.github.com>
2026-09-24 21:41:38 -0700

    Update GitHub owner and repository names in save.js

--- functions/admin/api/save.js
+++ functions/admin/api/save.js
@@ -1,6 +1,6 @@
 export async function onRequestPost(context) {
-  const OWNER = "your-github-username";   // CHANGE ME
-  const REPO  = "your-repo-name";         // CHANGE ME
+  const OWNER = "univyrsal";   // CHANGE ME
+  const REPO  = "school-from-scratch";         // CHANGE ME
   const PATH  = "cms-test.txt";           // throwaway test file
 
   const url = `https://api.github.com/repos/${OWNER}/${REPO}/contents/${PATH}`;
